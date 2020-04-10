79662b8db4f007cde8673099f73514e7cc414bf3
Thiago Hickmann <>
2020-04-10 13:46:43 -0300

    CSV output for ONDEMAND working

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import json
+import csv
 
 # from urllib.request import urlopen
 # import gzip
@@ -9,8 +10,40 @@ import json
 #     source = gzip.decompress(response.read())
 # data = json.loads(source)
 
-with open('ecs-pricing.json') as f:
-    data = json.load(f)
+with open('ecs-pricing.json') as json_file:
+    data = json.load(json_file)
 
-print(type(data))
-print(data)
+region = 'sa-brazil-1'
+
+with open('ecs-pricing.csv', 'w', newline='') as csv_file:
+    fieldnames = [
+        'region',
+        'spec',
+        'cpu',
+        'ram',
+        'resourceSpecCode',
+        'image',
+        'imageSpec',
+        'chargeMode',
+        'productId',
+        'planListLength',
+        'amount'
+        ]
+    thewriter = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    thewriter.writeheader()
+    
+    
+    for resourceVM in data['product']['hws.resource.type.vm']:
+        thewriter.writerow({
+            'region' : region,
+            'spec' : resourceVM['spec'],
+            'resourceSpecCode' : resourceVM['resourceSpecCode'],
+            'image' : resourceVM['image'],
+            'imageSpec' : resourceVM['imageSpec'],            
+            'cpu' : resourceVM['cpu'],
+            'ram' : resourceVM['mem'],
+            'chargeMode' : resourceVM['chargeMode'][0],
+            'planListLength' : len(resourceVM['planList']),
+            'productId' : resourceVM['planList'][0]['productId'],
+            'amount' : resourceVM['planList'][0]['amount']
+            })
