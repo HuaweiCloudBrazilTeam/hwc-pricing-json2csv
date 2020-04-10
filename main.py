1189ad13b5a6cb1e9df5bd588cbeb527fa2dad94
Thiago Hickmann <>
2020-04-10 18:23:11 -0300

    RI CSV output working, before refactor

--- main.py
+++ main.py
@@ -10,40 +10,55 @@ import csv
 #     source = gzip.decompress(response.read())
 # data = json.loads(source)
 
-with open('ecs-pricing.json') as json_file:
+with open("ecs-pricing.json") as json_file:
     data = json.load(json_file)
 
-region = 'sa-brazil-1'
+region = "sa-brazil-1"
 
-with open('ecs-pricing.csv', 'w', newline='') as csv_file:
+with open("ecs-pricing.csv", "w", newline="") as csv_file:
     fieldnames = [
-        'region',
-        'spec',
-        'cpu',
-        'ram',
-        'resourceSpecCode',
-        'image',
-        'imageSpec',
-        'chargeMode',
-        'productId',
-        'planListLength',
-        'amount'
-        ]
+        "region",
+        "generation",
+        "spec",
+        "specSize",
+        "performType",
+        "cpu",
+        "ram",
+        "resourceSpecCode",
+        "image",
+        "imageSpec",
+        "chargeMode",
+        "productId",
+        "planListLength",
+        "paymentTypeKey",
+        "amount",
+    ]
     thewriter = csv.DictWriter(csv_file, fieldnames=fieldnames)
     thewriter.writeheader()
-    
-    
-    for resourceVM in data['product']['hws.resource.type.vm']:
-        thewriter.writerow({
-            'region' : region,
-            'spec' : resourceVM['spec'],
-            'resourceSpecCode' : resourceVM['resourceSpecCode'],
-            'image' : resourceVM['image'],
-            'imageSpec' : resourceVM['imageSpec'],            
-            'cpu' : resourceVM['cpu'],
-            'ram' : resourceVM['mem'],
-            'chargeMode' : resourceVM['chargeMode'][0],
-            'planListLength' : len(resourceVM['planList']),
-            'productId' : resourceVM['planList'][0]['productId'],
-            'amount' : resourceVM['planList'][0]['amount']
-            })
+
+    for resourceVM in data["product"]["hws.resource.type.vm"]:
+        pricing_entry = {
+            "region": region,
+            "generation": resourceVM["generation"].lower(),
+            "spec": resourceVM["spec"],
+            "specSize": resourceVM["spec"].split(".", 1)[1],
+            "resourceSpecCode": resourceVM["resourceSpecCode"],
+            "image": resourceVM["image"],
+            "imageSpec": resourceVM["imageSpec"],
+            "performType": resourceVM["performType"],
+            "cpu": resourceVM["cpu"].split()[0],
+            "ram": resourceVM["mem"].split()[0],
+            "planListLength": len(resourceVM["planList"]),
+            "chargeMode": resourceVM["chargeMode"][0],
+        }
+        if resourceVM["chargeMode"][0] == "ONDEMAND":
+            pricing_entry["productId"] = resourceVM["planList"][0]["productId"]
+            pricing_entry["amount"] = resourceVM["planList"][0]["amount"]
+            thewriter.writerow(pricing_entry)
+        else:
+            for plan in resourceVM["planList"]:
+                pricing_entry["paymentTypeKey"] = plan["paymentTypeKey"]
+                pricing_entry["productId"] = plan["productId"]
+                pricing_entry["amount"] = plan["amount"]
+
+                thewriter.writerow(pricing_entry)
